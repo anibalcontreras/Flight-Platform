9143002c1d6af04e49f2e50bb0b818aeecb3ab06
ANIBAL CONTRERAS <anibal.contreras@uc.cl>
2022-05-16 20:19:04 -0400

    consulta1

--- Entrega2/queries/consulta1.php
+++ Entrega2/queries/consulta1.php
@@ -5,10 +5,27 @@
 <?php
     #Llama a conexión, crea el objeto PDO y obtiene la variable $db
     require("../config/conexion.php");
-
-        // HACER CONSULTA
+    
+        $query = "SELECT vuelo_id, codigo_vuelo, estado FROM vuelos WHERE estado = 'pendiente';";    
+        $result = $db -> prepare($query);
+        $result = execute();
+        $estados = $result -> fetchAll();
     ?>
-
+      <table>
+    <tr>
+      <th>ID</th>
+      <th>Código</th>
+      <th>Estado</th>
+    </tr>
+  
+      <?php
+        // echo $pokemones;
+        foreach ($estados as $e) {
+          echo "<tr><td>$e[0]</td><td>$e[1]</td><td>$e[2]</td></tr>";
+      }
+      ?>
+      
+  </table>
 
 
 <?php include('../templates/footer.html'); ?>
